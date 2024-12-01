fcdbf3d6f1fc8462c477cd1c9fe9e21da9bf9dd8
Aditya <adimail2404@gmail.com>
2024-12-01 11:55:42 +0530

    script: add problem link in header

--- new.pl
+++ new.pl
@@ -7,26 +7,28 @@ if ( @ARGV == 2 ) {
 	my $year    = $ARGV[0];
 	my $problem = $ARGV[1];
 
+	$problem =~ s/^0+//g;
+
 	my $dir = "$year/$problem";
 
 	make_path($dir) or die "Failed to create directory $dir: $!\n";
 
 	open my $input_file, '>', "$dir/input.txt"
 	  or die "Could not create input.txt: $!\n";
-	open my $que_file, '>', "$dir/que.txt"
-	  or die "Could not create que.txt: $!\n";
 	open my $sol_file, '>', "$dir/sol.pl"
 	  or die "Could not create sol.pl: $!\n";
 	open my $sol2_file, '>', "$dir/sol2.pl"
 	  or die "Could not create sol2.pl: $!\n";
 
-	print $sol_file
-	  "open my \$file, '<', 'input.txt' or die \"Could not open file: \$!\";\n";
-	print $sol2_file
-	  "open my \$file, '<', 'input.txt' or die \"Could not open file: \$!\";\n";
+	my $header =
+	  "# Problem statement: https://adventofcode.com/$year/day/$problem\n\n";
+	$header = $header
+	  . "open my \$file, '<', 'input.txt' or die \"could not open file: \$!\";\n";
+
+	print $sol_file $header;
+	print $sol2_file $header;
 
 	close $input_file;
-	close $que_file;
 	close $sol_file;
 	close $sol2_file;
 
